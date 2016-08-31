ad7026be0e87b44ddfc4cc7b848c738565bef248
Lugash <lucasgava1993@gmail.com>
2016-08-31 23:06:23 +0200

    final-september
    - fix minor bug
    - redesign
    - outline fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,7 +86,7 @@ module.exports = function( grunt ) {
 
     grunt.registerTask( "work", [
         "build",
-        "analyse",
+        // "analyse",
         "browserSync",
         "watch"
     ] );
